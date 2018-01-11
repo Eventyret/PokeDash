84b5b7ae24e8c9a780de44abd7ca01d63833fe50
Simen Daehlin <simen.dehlin@gmail.com>
2018-01-11 15:36:33 +0000

    changed the limit

--- src/app/services/pokemon-data.service.ts
+++ src/app/services/pokemon-data.service.ts
@@ -8,7 +8,7 @@ export class PokemonDataService {
   constructor(private http: Http) { }
 
   getPokemons() {
-    return this.http.get("https://api.dehlin.info/api/1.1/tables/pokemon/rows?limit=800")
+    return this.http.get("https://api.dehlin.info/api/1.1/tables/pokemon/rows?limit=200")
     .map(res => res.json())
   }
 
