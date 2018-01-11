e33742875f0a3767a3a58095339805993aca69c5
Simen Daehlin <simen.dehlin@gmail.com>
2018-01-11 10:27:25 +0000

    Added battle page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ChartsModule } from 'ng2-charts';
 import { BarChartComponent } from './components/charts/bar-chart/bar-chart.component';
 import { LineChartComponent } from './components/charts/line-chart/line-chart.component';
 import { PieChartComponent } from './components/charts/pie-chart/pie-chart.component';
+import { BattleComponent } from './sections/battle/battle.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { PieChartComponent } from './components/charts/pie-chart/pie-chart.compo
     PokemonListComponent,
     BarChartComponent,
     LineChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    BattleComponent
   ],
   imports: [
     BrowserModule,
