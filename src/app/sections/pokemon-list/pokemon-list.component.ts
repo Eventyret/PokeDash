4a473c9842a6280d7ec3ec01e5c85dea3dfe973a
Simen Daehlin <simen.dehlin@gmail.com>
2018-01-11 10:22:15 +0000

    Added mockdata

--- src/app/sections/pokemon-list/pokemon-list.component.ts
+++ src/app/sections/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemons } from '../../components/shared/pokemons'
 
 @Component({
-  selector: 'app-pokemon-list',
-  templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  selector: "app-pokemon-list",
+  templateUrl: "./pokemon-list.component.html",
+  styleUrls: ["./pokemon-list.component.css"]
 })
 export class PokemonListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  pokemons: Pokemons[] = [
+    { id: 1, name: "Pokemon 1", japanese_name: "Pokemon Japanese" },
+    { id: 2, name: "Pokemon 1", japanese_name: "Pokemon Japanese" },
+    { id: 3, name: "Pokemon 1", japanese_name: "Pokemon Japanese" },
+    { id: 4, name: "Pokemon 1", japanese_name: "Pokemon Japanese" },
+    { id: 5, name: "Pokemon 1", japanese_name: "Pokemon Japanese" },
+    { id: 6, name: "Pokemon 1", japanese_name: "Pokemon Japanese" },
+    { id: 7, name: "Pokemon 1", japanese_name: "Pokemon Japanese" },
+    { id: 8, name: "Pokemon 1", japanese_name: "Pokemon Japanese" },
+    { id: 9, name: "Pokemon 1", japanese_name: "Pokemon Japanese" },
+    { id: 10, name: "Pokemon 1", japanese_name: "Pokemon Japanese" }
+  ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
