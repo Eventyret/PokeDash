f867cf3a5511af9720146d01518005b243101816
Simen Daehlin <simen.dehlin@gmail.com>
2018-01-11 19:57:02 +0000

    cleanup

--- src/app/sections/dashboard/dashboard.component.ts
+++ src/app/sections/dashboard/dashboard.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  types: any[];
+  pokemons: any[];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
