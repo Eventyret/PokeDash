cdb5a9ca82ca0b3f2f9c6591e2e20933b27fe096
Simen Daehlin <simen.dehlin@gmail.com>
2018-01-11 15:32:27 +0000

    Added function get get the services and return the data

--- src/app/services/pokemon-data.service.ts
+++ src/app/services/pokemon-data.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PokemonDataService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getPokemons() {
+    return this.http.get("https://api.dehlin.info/api/1.1/tables/pokemon/rows?limit=800")
+    .map(res => res.json())
+  }
 
 }
