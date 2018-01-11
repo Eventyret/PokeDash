12c925c797e890fed6647dbcd96dbf97bf3bfd8e
Simen Daehlin <simen.dehlin@gmail.com>
2018-01-11 10:28:44 +0000

    Added routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { BattleComponent } from './app/sections/battle/battle.component';
 import { DashboardComponent } from './app/sections/dashboard/dashboard.component';
 import { PokemonListComponent } from './app/sections/pokemon-list/pokemon-list.component';
 import { Routes } from '@angular/router';
@@ -5,6 +6,7 @@ import { Routes } from '@angular/router';
 export const appRoutes: Routes = [
 	{path: 'dashboard', component: DashboardComponent },
 	{path: 'list', component: PokemonListComponent },
+	{path: 'battle-status', component: BattleComponent },
 
 	{path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
