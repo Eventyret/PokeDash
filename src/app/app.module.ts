8b410c00123ada9d7d0374134768b1c890261a5e
Simen Daehlin <simen.dehlin@gmail.com>
2018-01-13 15:34:57 +0000

    added formsmodule for filtering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { PokemonDataService } from './services/pokemon-data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -39,7 +39,7 @@ import { DataFilterPipe } from './components/shared/data-filter.pipe';
     PaginationComponent,
     DataFilterPipe
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), ChartsModule, HttpModule, DataTableModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), ChartsModule, HttpModule, DataTableModule, FormsModule],
   providers: [PokemonDataService],
   bootstrap: [AppComponent]
 })
