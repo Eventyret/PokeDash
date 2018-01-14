c8b9ff3022b5384817187508434d9a7e9790f728
Simen Daehlin <simen.dehlin@gmail.com>
2018-01-14 01:40:08 +0000

    Started styling. Logic works

--- src/app/components/battles/battles.component.ts
+++ src/app/components/battles/battles.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { Battle } from '../shared/battle';
+import { Battle } from "../shared/battle";
 import * as _ from "lodash";
-
-
 
 @Component({
   selector: "app-battles",
@@ -11,30 +9,60 @@ import * as _ from "lodash";
 })
 export class BattlesComponent implements OnInit {
   constructor() {}
-  
+
   color: string;
   buttonText: string;
 
   @Input() battleInput: Battle;
   @Input() battleNumber: number;
   Battles: any[];
+  isLoading = false;
+  firstBattle = true;
+  winners = [];
 
   ngOnInit() {
     this.setBattleStatus(this.battleInput.isActive);
-    this.Battles = _.castArray(this.battleInput)
-    this.theWinner();
+    this.Battles = _.castArray(this.battleInput);
   }
-
 
   setBattleStatus(isInBattle: boolean) {
     if (isInBattle) {
-      this.battleInput.isActive = true;
-      this.color = "#66BB6A";
-      this.buttonText = "Stop Fight";
+      this.fighting();
+      setTimeout(() => {
+        this.battleInput.isActive = true;
+        this.color = "#66BB6A";
+        this.buttonText = "Stop Fight";
+        this.isLoading = false;
+      }, 3000);
     } else {
-      this.battleInput.isActive = false;
-      this.color = "#ff6b6b";
-      this.buttonText = "Activate Fight";
+      this.stopFighting();
+      setTimeout(() => {
+        this.battleInput.isActive = false;
+        this.color = "#ff6b6b";
+        this.buttonText = "Activate Fight";
+        this.isLoading = false;
+      }, 3000);
+    }
+  }
+
+  fighting() {
+    this.color = "#FFCA28";
+    this.buttonText = "Fight in progress....";
+    this.isLoading = true;
+    this.winners = [];
+  }
+  stopFighting() {
+    if (!this.firstBattle) {
+      this.color = "#FFCA28";
+      this.buttonText = "Checking Winner";
+      this.isLoading = true;
+      this.firstBattle = false;
+      this.theWinner();
+    } else {
+      this.color = "#FFCA28";
+      this.buttonText = "Getting Status";
+      this.firstBattle = false;
+      this.isLoading = true;
     }
   }
 
@@ -42,14 +70,13 @@ export class BattlesComponent implements OnInit {
     this.setBattleStatus(!battleStatus);
   }
 
-    theWinner() {
-    let winners = [];
+  theWinner() {
     this.Battles.forEach(battle => {
-      let winnerNumber = Math.floor(Math.random()  *10);
-     if (winnerNumber  > 5) {
-        winners.push(battle["pokemon1"]);
+      let winnerNumber = Math.floor(Math.random() * 10);
+      if (winnerNumber > 5) {
+        this.winners.push(battle["pokemon1"]);
       } else {
-        winners.push(battle["pokemon2"]);
+        this.winners.push(battle["pokemon2"]);
       }
     });
   }
