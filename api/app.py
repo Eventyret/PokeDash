caf12f634a55a42a4384fe648dd1826d796620ed
Simen Daehlin <sdaehlin@INTERGAGE2.LOCAL>
2018-08-01 14:22:30 +0100

    updated app.py

--- api/app.py
+++ api/app.py
@@ -1,11 +1,19 @@
+import os
 from flask import Flask, jsonify, request
+from dotenv import load_dotenv
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 
-app = Flask(__name__)
+# load dotenv in the base root
+# refers to application_top
+APP_ROOT = os.path.join(os.path.dirname(__file__), '.')
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
+SECRET_URI = os.getenv("SECRET_URI")
 
+app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "heroku_gtwrqfln"
-app.config["MONGO_URI"] = "mongodb://dbuser:FIIWhpYSO1JV8nX@ds161751.mlab.com:61751/heroku_gtwrqfln"
+app.config["MONGO_URI"] = SECRET_URI
 CORS(app)
 
 mongo = PyMongo(app)
@@ -16,7 +24,8 @@ def get_all_pokemons():
     pokemons = mongo.db.pokemons
     output = []
     for q in pokemons.find():
-        output.append({"PokedexID": q["PokeID"], "Name": q["Name"], "Type1": q["Type1"], "Type2": q["Type2"], "Type2": q["Type2"], "Total": q["Total"], "HP": q["HP"], "Attack": q["Attack"], "Defense": q["Defense"], "Sp": q ["Sp"], "Speed": q["Speed"], "Generation": q["Generation"], "Legendary": q["Legendary"], "Battle": q["Battle"]})
+        output.append({"PokedexID": q["PokeID"], "Name": q["Name"], "Type1": q["Type1"], "Type2": q["Type2"], "Type2": q["Type2"], "Total": q["Total"], "HP": q["HP"],
+                       "Attack": q["Attack"], "Defense": q["Defense"], "Sp": q["Sp"], "Speed": q["Speed"], "Generation": q["Generation"], "Legendary": q["Legendary"], "Battle": q["Battle"]})
 
     return jsonify({"results": output})
 
