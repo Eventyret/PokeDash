d7bd022097c7cdb366b25d7507d1ac91607f6f0e
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-16 00:32:50 +0100

    updated to add dev mode for local testing

--- api/app.py
+++ api/app.py
@@ -4,10 +4,15 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS
 
 SECRET_URI = os.getenv("SECRET_URI")
+dev = True
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "heroku_gtwrqfln"
-app.config["MONGO_URI"] = SECRET_URI
+if not dev:
+    app.config["MONGO_URI"] = SECRET_URI
+else:
+    app.config["MONGO_URI"] = "mongodb://api_dev_user:6L4Ltv7yFuKwguA@ds161751.mlab.com:61751/heroku_gtwrqfln"
+print(SECRET_URI)
 CORS(app)
 mongo = PyMongo(app)
 
@@ -19,11 +24,24 @@ def lets_redirect():
 
 @app.route("/api", methods=["GET"])
 def get_all_pokemons():
-    pokemons = mongo.db.pokemons
+    if not dev:
+        pokemons = mongo.db.pokemons
+    else:
+        pokemons = mongo.db.pokemons_dev
     output = []
     for q in pokemons.find():
-        output.append({"PokedexID": q["PokeID"], "Name": q["Name"], "Type1": q["Type1"], "Type2": q["Type2"], "Type2": q["Type2"], "Total": q["Total"], "HP": q["HP"],
-                       "Attack": q["Attack"], "Defense": q["Defense"], "Sp": q["Sp"], "Speed": q["Speed"], "Generation": q["Generation"], "Legendary": q["Legendary"], "Battle": q["Battle"], "Sprite": q["Sprite"]})
+        if dev:
+            output.append({"PokedexID": q["PokeID"], "Name": q["Name"], "Type1": q["Type1"], "Type2": q["Type2"],
+                           "Type2": q["Type2"], "Total": q["Total"], "HP": q["HP"],
+                           "Attack": q["Attack"], "Defense": q["Defense"], "Sp": q["Sp"], "Speed": q["Speed"],
+                           "Generation": q["Generation"], "Legendary": q["Legendary"], "Battle": q["Battle"],
+                           "Sprite": q["Sprite"], "DeveloperMode": q["DeveloperMode"]})
+        else:
+            output.append({"PokedexID": q["PokeID"], "Name": q["Name"], "Type1": q["Type1"], "Type2": q["Type2"],
+                           "Type2": q["Type2"], "Total": q["Total"], "HP": q["HP"],
+                           "Attack": q["Attack"], "Defense": q["Defense"], "Sp": q["Sp"], "Speed": q["Speed"],
+                           "Generation": q["Generation"], "Legendary": q["Legendary"], "Battle": q["Battle"],
+                           "Sprite": q["Sprite"]})
 
     return jsonify({"results": output})
 
