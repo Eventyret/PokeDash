4de0379698aaf1e68d3a3b2bba55e67d30aebb28
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-16 18:48:39 +0100

    Added editorconfig for vscode, added test unit file. Updated requiremetns

--- api/app.py
+++ api/app.py
@@ -3,16 +3,15 @@ from flask import Flask, jsonify, request, redirect
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 
+DeveloperMode = True
 SECRET_URI = os.getenv("SECRET_URI")
-dev = False
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "heroku_gtwrqfln"
-if not dev:
-    app.config["MONGO_URI"] = SECRET_URI
-else:
+if DeveloperMode:
     app.config["MONGO_URI"] = "mongodb://api_dev_user:6L4Ltv7yFuKwguA@ds161751.mlab.com:61751/heroku_gtwrqfln"
-print(SECRET_URI)
+else:
+    app.config["MONGO_URI"] = SECRET_URI
 CORS(app)
 mongo = PyMongo(app)
 
@@ -24,13 +23,13 @@ def lets_redirect():
 
 @app.route("/api", methods=["GET"])
 def get_all_pokemons():
-    if not dev:
-        pokemons = mongo.db.pokemons
-    else:
+    if DeveloperMode:
         pokemons = mongo.db.pokemons_dev
+    else:
+        pokemons = mongo.db.pokemons
     output = []
     for q in pokemons.find():
-        if dev:
+        if DeveloperMode:
             output.append({"PokedexID": q["PokeID"], "Name": q["Name"], "Type1": q["Type1"], "Type2": q["Type2"],
                            "Type2": q["Type2"], "Total": q["Total"], "HP": q["HP"],
                            "Attack": q["Attack"], "Defense": q["Defense"], "Sp": q["Sp"], "Speed": q["Speed"],
@@ -52,4 +51,4 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=DeveloperMode)
