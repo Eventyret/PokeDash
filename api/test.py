4caf1f779817eea9d0651d774f61d68f90345d4c
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-16 21:59:37 +0100

    unit test completed

--- api/test.py
+++ api/test.py
@@ -6,8 +6,10 @@ class ApiTestCase(unittest.TestCase):
 
     def test_api(self):
         tester = app.test_client(self)
+        print("=> Testing to see if API responds")
         response = tester.get("/api", content_type="application/json")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200):
+
 
 if __name__ == "__main__":
     unittest.main()
