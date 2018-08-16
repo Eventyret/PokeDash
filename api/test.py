3d26e36ab5637d3f733d46734ea689de36f3f7ff
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-16 21:29:22 +0100

    written unit test to see if it grabs the correct data and server is running

---
+++ api/test.py
@@ -0,0 +1,13 @@
+from app import app
+import unittest
+
+
+class ApiTestCase(unittest.TestCase):
+
+    def test_api(self):
+        tester = app.test_client(self)
+        response = tester.get("/api", content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
