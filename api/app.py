e89ed7190ac3cc17f9c48d84a5e6db8d5b502d65
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-21 21:37:28 +0100

    added default

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS
 
 
-DeveloperMode = os.getenv("Development", True)
+DeveloperMode = os.getenv("Development", default=True)
 Docker = os.getenv("docker")
 SECRET_URI = os.getenv("SECRET_URI")
 
